edbe66b753997d0cb2cb3a2906ff1e2f76fac882
DENGEKI-TDU <115410767+DENGEKI-TDU@users.noreply.github.com>
2022-12-09 02:00:17 +0900

    Update latest.js

--- assets/js/latest.js
+++ assets/js/latest.js
@@ -1,9 +1,9 @@
-let title01 = '最近のでんげき'
+let title01 = 'お引越しだｺﾝ。'
 let year01 = '2022'
-let month01 = '11'
-let day1 = '25'
+let month01 = '12'
+let day1 = '9'
 let writer01 = 'ベレト'
-let tag01 = 'きつねの日常'
+let tag01 = 'きつねからのお知らせ'
 
 let t01 = document.getElementById('No01').innerHTML;
 document.getElementById('No01').innerHTML = '<a href="https://dengeki-tdu.github.io/DENGEKI/article/fox/'+year01+'/'+month01+'/'+month01+day1+'.html" target="_parent"><h1>'+title01+'</h1></a>';
