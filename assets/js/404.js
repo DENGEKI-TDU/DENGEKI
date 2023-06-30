e498d1923ff25e4c67687c1812b4f2a773ca1f11
DENGEKI-TDU <115410767+DENGEKI-TDU@users.noreply.github.com>
2023-07-01 01:34:43 +0900

    Add files via upload

---
+++ assets/js/404.js
@@ -0,0 +1,15 @@
+num = Math.floor(Math.random() *100) + 1;
+
+let content = document.getElementById("404");
+
+if(num >=1 && num<=49){
+    content.innerHTML = '<h1>404</h1><h3>存在しないページだｺﾝよ～～</h3><p>ホームにはここかメニューから戻れるｺﾝ!!<a href=\"index.html\">HOME</a></p><img src=\"https://dengeki-tdu.github.io/DENGEKI/assets/img/fox1.jpg\"></img>';
+}
+
+else if(num >= 50 && num <=98){
+    content.innerHTML = '<h1>404</h1><h3>残念ながらこのページは存在しないｺﾝよ～～</h3><p>ホームにはここかメニューから戻れるｺﾝ!!<a href=\"index.html\">HOME</a><br>ごめんね～～</p><img src=\"https://dengeki-tdu.github.io/DENGEKI/assets/img/fox2.jpg\"></img>';
+}
+
+else{
+    content.innerHTML = '<h1>404</h1><h3>キミはどうやってこのページにたどり着いたｺﾝかな？<br>残念だけどこのページは存在してないｺﾝ。</h3><p>ホームにはここかメニューから戻れるｺﾝ!!<a href=\"index.html\">HOME</a><br>ごめんね～～</p><img src=\"https://dengeki-tdu.github.io/DENGEKI/assets/img/fox2.jpg\"></img>';
+}
